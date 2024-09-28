d63a5412c8b8b81f679e9d2615c1488befce8b3a
NJesus144 <nalbert-edm@outlook.com>
2024-09-28 18:48:43 -0300

    Add login and register pages with respective forms

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,10 @@
+import RegisterForm from "../../../components/RegisterForm";
+import Link from "next/link";
+
+export default function Register() {
+  return (
+    <div className="bg-background-primary h-screen w-full pt-20">
+      <RegisterForm />
+    </div>
+  )
+}
