c89aeb13f20a44388be80f785ecb21e28954fc04
NJesus144 <nalbert-edm@outlook.com>
2024-09-29 09:02:13 -0300

    create register page

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,9 +1,8 @@
-import RegisterForm from "../../../components/RegisterForm";
-import Link from "next/link";
+import RegisterForm from "../../../components/RegisterForm"
 
 export default function Register() {
   return (
-    <div className="bg-background-primary h-screen w-full pt-20">
+    <div className="bg-darkGray h-screen w-full pt-20">
       <RegisterForm />
     </div>
   )
