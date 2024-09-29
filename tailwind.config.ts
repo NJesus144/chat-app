97a6793af4d0c51e52cf986aea01cdd78b3a39a4
NJesus144 <nalbert-edm@outlook.com>
2024-09-29 09:01:42 -0300

    add theme in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,42 +1,75 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: {
-          light: '#9F7AEA', // Roxo claro
-          DEFAULT: '#7C3AED', // Roxo principal
-          dark20: '#8B5CF6',
-          dark: '#6B21A8', // Roxo escuro
-        },
-        secondary: {
-          light: '#4B5563', // Cinza claro
-          DEFAULT: '#1F2937', // Cinza principal
-          dark: '#111827', // Cinza escuro
-        },
-        background: {
-          primary: '#111827', // Background dark principal
-          secondary: '#1E1E2E', // Background dark secundário (custom)
-        },
-        text: {
-          primary: '#E5E7EB', // Texto principal (cinza claro)
-          secondary: '#9CA3AF', // Texto secundário (cinza intermediário)
-        },
-        accent: {
-          DEFAULT: '#F472B6', // Acento rosa vibrante para contraste
-        },
-        backgroundImage: {
-          'gradient-purple-pink': 'linear-gradient(90deg, #8B5CF6 0%, #D946EF 100%)',
-        },
-      },
-    },
+  	extend: {
+  		colors: {
+  			neonPurple: '#9400D3',
+  			neonGreen: '#00FF00',
+  			neonYellow: '#FFFF00',
+  			neonPink: '#FF007F',
+				// neonPinkHover: '#ff006a',
+  			darkGray: '#1a1a1a',
+  			mediumGray: '#2b2b2b',
+  			lightGray: '#808080',
+  			cyan: '#00FFFF',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		fontFamily: {
+  			cyber: ['Roboto Mono"', 'monospace']
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
