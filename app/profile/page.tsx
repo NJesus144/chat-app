fceaa193e04dbf25a2e83563b54044d08af688fe
NJesus144 <nalbert-edm@outlook.com>
2024-10-10 20:33:26 -0300

    Add ProfilePage component for displaying user profile information

---
+++ app/profile/page.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { getProfile } from '../../src/services/api'
+
+type dataType = {
+  username: string
+}
+export default function ProfilePage() {
+  const [profile, setProfile] = useState<dataType>()
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const token = localStorage.getItem('token')
+        if (token) {
+          const data = await getProfile(token)
+ 
+          setProfile(data)
+        } else {
+          setError('No token found')
+        }
+      } catch (err) {
+        setError('An unexpected error occurred')
+      }
+    }
+
+    fetchProfile()
+  }, [])
+
+  if (error) {
+    return <div className="text-red-500">{error}</div>
+  }
+
+  return (
+    <div className="max-w-md mx-auto mt-10">
+      <h2 className="text-2xl font-bold mb-5">Profile</h2>
+      {profile ? (
+        <div>
+          <p>
+            <strong>Username:</strong> {profile.username}
+          </p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  )
+}
